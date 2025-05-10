03c646ba94ac823e6ffe8aac0cf0ee00c991990e
rafia9005 <rafia9005@gmail.com>
2025-05-10 13:48:10 +0000

    update

--- src/handler/admin/send-message.ts
+++ src/handler/admin/send-message.ts
@@ -1,25 +1,19 @@
 import { Ctx } from "@mengkodingan/ckptw";
-import { ENV } from "../../env";
+import { ENV } from "../../env";  // Pastikan ENV berisi variabel yang dibutuhkan
 
 const GeminiHandler = {
     name: "message",
     code: async (ctx: Ctx) => {
-        const message = ctx.msg.key.participant;
-
-        if (message) {
-            const parts = message.split(" ");
-            const userNumber = parts[0];
-            const userMessage = parts.slice(1).join(" ");
-
-            const authorNumber = ENV.AUTHOR;
-
-            if (userNumber === authorNumber) {
-                await ctx.reply(`Selamat datang, Anda memiliki akses. Pesan Anda: ${userMessage}`);
-            } else {
-                await ctx.reply("Maaf, Anda tidak memiliki akses.");
-            }
+        const userNumber = ctx.msg.key.participant?.split('@')[0];
+        const authorNumber = ENV.AUTHOR;
+        console.log(userNumber)
+        if (userNumber === authorNumber) {
+            await ctx.reply(`Selamat datang, Anda memiliki akses ke prefix dengan kunci MONSTER_API`);
+        } else {
+            await ctx.reply("Maaf, Anda tidak memiliki akses ke prefix ini.");
         }
     },
 };
 
 module.exports = GeminiHandler;
+
